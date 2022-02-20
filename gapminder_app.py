1b1b8980a3c78f0be9df3963caf509375320072a
Anupriya Srivastava <anupriya.g.srivastava@gmail.com>
2022-02-19 17:00:05 -0800

    ver_1

---
+++ gapminder_app.py
@@ -0,0 +1,35 @@
+from dash import Dash, dcc, html, Input, Output
+import pandas as pd
+import altair as alt
+
+# Read the dats
+gm = pd.read_csv('data/gapminder_2018.csv', parse_dates=['year'])
+gm_2018 = gm[gm['year'] == '2018-01-01']
+
+# Set-up the app and user interface
+gapminder_app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+
+gapminder_app.layout = html.Div([
+    html.Iframe(
+        id='scatter',
+        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+    dcc.Dropdown(
+        id='xcol-widget', value='children_per_woman',
+        options=[{'label': col, 'value': col} for col in gm_2018.columns])])
+
+# Setup callbacks
+@gapminder_app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('xcol-widget', 'value'))
+
+def plot_chart(xcol):
+    chart = alt.Chart(gm_2018).mark_point(filled=True).encode(
+        x=xcol,
+        y='life_expectancy',
+        tooltip='country',
+        color='region',
+        size='population').interactive()
+    return chart.to_html()
+
+if __name__ == '__main__':
+    gapminder_app.run_server(debug=True)
